c0de76538f944f0201b63dc47b6bdb4dc2a1c39d
DanendraF <email.lenovoipslim5i@gmail.com>
2025-12-24 17:19:43 +0700

    Implement dynamic import for App component to disable server-side rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
-import App from '../src/App'
+import dynamic from "next/dynamic";
+
+const App = dynamic(() => import("../src/App"), {
+  ssr: false,
+});
 
 export default function Home() {
-  return <App />
+  return <App />;
 }
